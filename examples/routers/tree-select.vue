00879902b96427b8c9b0719cfa66de7c13465e5d
杨振 <yangzhen1262@163.com>
2023-12-11 11:57:03 +0800

    修改tree和tree-select组件

--- examples/routers/tree-select.vue
+++ examples/routers/tree-select.vue
@@ -1,65 +1,77 @@
 <template>
-    <TreeSelect v-model="value" :data="data" v-width="200" />
+    <TreeSelect
+        v-model="value"
+        multiple
+        showCheckbox
+        :data="data"
+        v-width="200"
+        @on-change="handleChange"
+    />
 </template>
 <script>
-    export default {
-        data () {
-            return {
-                value: '',
-                data: [
-                    {
-                        title: 'parent1',
-                        expand: true,
-                        value: 'parent1',
-                        selected: false,
-                        checked: false,
-                        children: [
-                            {
-                                title: 'parent 1-1',
-                                expand: true,
-                                value: 'parent1-1',
-                                selected: false,
-                                checked: false,
-                                children: [
-                                    {
-                                        title: 'leaf 1-1-1',
-                                        value: 'leaf1',
-                                        selected: false,
-                                        checked: false,
-                                    },
-                                    {
-                                        title: 'leaf 1-1-2',
-                                        value: 'leaf2',
-                                        selected: false,
-                                        checked: false,
-                                    }
-                                ]
-                            },
-                            {
-                                title: 'parent 1-2',
-                                expand: true,
-                                value: 'parent1-2',
-                                selected: false,
-                                checked: false,
-                                children: [
-                                    {
-                                        title: 'leaf 1-2-1',
-                                        value: 'leaf3',
-                                        selected: false,
-                                        checked: false,
-                                    },
-                                    {
-                                        title: 'leaf 1-2-1',
-                                        value: 'leaf4',
-                                        selected: false,
-                                        checked: false,
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
-    }
+export default {
+    data() {
+        return {
+            value: "",
+            data: [
+                {
+                    title: "测试1",
+                    expand: true,
+                    value: "parent1",
+                    selected: false,
+                    checked: false,
+                    children: [
+                        {
+                            title: "测试 1-1",
+                            expand: true,
+                            value: "parent1-1",
+                            selected: false,
+                            checked: false,
+                            children: [
+                                {
+                                    title: "测试 1-1-1",
+                                    value: "leaf1",
+                                    selected: false,
+                                    checked: false,
+                                },
+                                {
+                                    title: "测试 1-1-2",
+                                    value: "leaf2",
+                                    selected: false,
+                                    checked: false,
+                                },
+                            ],
+                        },
+                        {
+                            title: "测试 1-2",
+                            expand: true,
+                            value: "leaf 1-2",
+                            selected: false,
+                            checked: false,
+                            children: [
+                                {
+                                    title: "测试 1-2-1",
+                                    value: "leaf3",
+                                    selected: false,
+                                    checked: false,
+                                },
+                                {
+                                    title: "测试 1-2-1",
+                                    value: "leaf4",
+                                    selected: false,
+                                    checked: false,
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        };
+    },
+    methods: {
+        handleChange(val) {
+            console.log(val);
+        },
+    },
+};
 </script>
